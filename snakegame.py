62c4f2d01ebb6ae95efdd83d5a1da8d441c4ccff
Dikshant Khatri <khatridikshant@outlook.com>
2026-04-30 12:09:26 +0545

    Snake Controls Added

--- snakegame.py
+++ snakegame.py
@@ -18,16 +18,28 @@ direction = "RIGHT"
 while True:
     
     for event in pygame.event.get():
+    
         if event.type == pygame.QUIT:
             pygame.quit()
+    
             sys.exit()
+    
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP and direction != 'DOWN':
+                direction = 'UP'
+            elif event.key == pygame.K_DOWN and direction != 'UP':
+                direction = "DOWN"
+            elif event.key == pygame.K_LEFT and direction != 'RIGHT':
+                direction = 'LEFT'
+            elif event.key == pygame.K_RIGHT and direction != 'LEFT':
+                direction = 'RIGHT'    
           
-    head_x, head_y = snake[0]
+    head_x,head_y = snake[0]
  
     if direction == 'RIGHT':
         head_x += block_size
     elif direction == 'LEFT':
-        head_y -= block_size
+        head_x -= block_size
     elif direction == 'UP':
         head_y -= block_size
     elif direction == 'DOWN':
@@ -39,14 +51,12 @@ while True:
  
     
     screen.fill((0,0,0))
-
-    
  
     for block in snake:
         pygame.draw.rect(screen,(0,255,0),(block[0],block[1],block_size,block_size))       
  
     pygame.display.update()
  
-    clock.tick(15)
+    clock.tick(10)
     
             
