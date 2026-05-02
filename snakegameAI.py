aeebc6aa3d2835436931d7ae523d1b80a5cc3524
Dikshant Khatri <khatridikshant@outlook.com>
2026-05-02 21:07:01 +0545

    Reset Function Added

--- snakegameAI.py
+++ snakegameAI.py
@@ -10,3 +10,21 @@ BLOCK_SIZE = 10
 ACTION_SPACE = [0,1,2]
 
 DIRECTIONS = ["UP","RIGHT","DOWN","LEFT"]
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+snake = []
+direction = "RIGHT"
+food = (0,0)
+score = 0
+
+def reset():
+    
+    global snake,direction,food,score
+    
+    snake = [(100,100),(90,100),(80,100)]
+    
+    direction = "RIGHT"
+    score = 0
+    
